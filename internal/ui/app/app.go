4331cf4bc565dcf7c412599dd6c13d90c4f7698a
Alex <alex@emery.xyz>
2023-10-09 21:53:12 +0100

    refactor(app): removed channels
    The cmds execute in goroutines, they don't need the channels

--- internal/ui/app/app.go
+++ internal/ui/app/app.go
@@ -22,118 +22,117 @@ func New(montui *montui.Montui) *App {
 func (c *App) Update(msg tea.Msg) tea.Cmd {
 	switch msg := msg.(type) {
 	case FetchTransactionsMsg:
-		return Future(func(channel chan tea.Msg) {
-			transactions, _ := c.montui.FetchTransactions(c.ctx, "", nil, nil)
-			channel <- NewTransactionsMsg{Transactions: transactions}
-		})
+		return func() tea.Msg {
+			transactions, err := c.montui.FetchTransactions(c.ctx, "", nil, nil)
+			if err != nil {
+				return NewErrorMsg(err)
+			}
+			return NewTransactionsMsg{Transactions: transactions}
 
+		}
 	case GetTransactionsMsg:
-		return Future(func(channel chan tea.Msg) {
-			transactions, _ := c.montui.GetTransactions("", nil, nil)
-			channel <- NewTransactionsMsg{Transactions: transactions}
-		})
+		return func() tea.Msg {
+			transactions, err := c.montui.GetTransactions("", nil, nil)
+			if err != nil {
+				return NewErrorMsg(err)
+			}
+			return NewTransactionsMsg{Transactions: transactions}
+		}
 
 	case GetCategoriesMsg:
-		return Future(func(channel chan tea.Msg) {
+		return func() tea.Msg {
 			categories, err := c.montui.GetCategories()
 			if err != nil {
-				channel <- NewCategoriesMsg{Categories: categories}
+				return NewCategoriesMsg{Categories: categories}
 			} else {
-				channel <- NewCategoriesMsg{Categories: categories}
+				return NewCategoriesMsg{Categories: categories}
 			}
-		})
+
+		}
 
 	case SetCategoryMsg:
-		return Future(func(channel chan tea.Msg) {
-			_, err := c.montui.SetCategory(msg.TransactionID, msg.CategoryID)
+		return func() tea.Msg {
+			categories, err := c.montui.GetCategories()
 			if err != nil {
-				channel <- NewErrorMsg(err)
+				return NewCategoriesMsg{Categories: categories}
 			} else {
-				channel <- GetTransactionsMsg{}
+				return NewCategoriesMsg{Categories: categories}
 			}
-		})
+		}
 	case UpdateCategoryMsg:
-		return Future(func(channel chan tea.Msg) {
+
+		return func() tea.Msg {
 			err := c.montui.UpdateCategory(msg.Category)
 			if err != nil {
-				channel <- NewErrorMsg(err)
-				return
+				return NewErrorMsg(err)
 			}
-			channel <- GetCategoriesMsg{}
-		})
+			return GetCategoriesMsg{}
+		}
 	case GetAccountsMsg:
-		return Future(func(channel chan tea.Msg) {
-			accounts, _ := c.montui.GetAccounts()
-			channel <- NewAccountsMsg{Accounts: accounts}
-		})
+		return func() tea.Msg {
+			accounts, err := c.montui.GetAccounts()
+			if err != nil {
+				return NewErrorMsg(err)
+			}
+			return NewAccountsMsg{Accounts: accounts}
+		}
+
 	case GetBanksMsg:
-		return Future(func(channel chan tea.Msg) {
-			banks, _ := c.montui.ListBanks()
-			channel <- NewBanksMsg{Banks: banks}
-		})
+		return func() tea.Msg {
+			banks, err := c.montui.ListBanks()
+			if err != nil {
+				return NewErrorMsg(err)
+			}
+			return NewBanksMsg{Banks: banks}
+		}
 	case CreateLinkMsg:
-		return Future(func(channel chan tea.Msg) {
+		return func() tea.Msg {
 			c.montui.Link(c.ctx, msg.BankID)
-			channel <- LinkReadyMsg{}
-		})
+			return LinkReadyMsg{}
+		}
 	case GetRulesMsg:
-		return Future(func(channel chan tea.Msg) {
+		return func() tea.Msg {
 			rules, err := c.montui.ListRules()
 			if err != nil {
-				channel <- NewErrorMsg(err)
-			} else {
-				channel <- NewRulesMsg{Rules: rules}
+				return NewErrorMsg(err)
 			}
-		})
+			return NewRulesMsg{Rules: rules}
+		}
 	case UpdateRuleMsg:
-		return Future(func(channel chan tea.Msg) {
+		return func() tea.Msg {
 			err := c.montui.UpdateRule(msg.Rule)
 			if err != nil {
-				channel <- NewErrorMsg(err)
+				return NewErrorMsg(err)
 			} else {
-				channel <- GetRulesMsg{}
+				return GetRulesMsg{}
 			}
-		})
+		}
 	case CreateRuleMsg:
-		return Future(func(channel chan tea.Msg) {
+		return func() tea.Msg {
 			err := c.montui.CreateRule(msg.Rule)
 			if err != nil {
-				channel <- NewErrorMsg(err)
-			} else {
-				channel <- GetRulesMsg{}
+				return NewErrorMsg(err)
 			}
-		})
+			return GetRulesMsg{}
+		}
 	case DeleteRuleMsg:
-		return Future(func(channel chan tea.Msg) {
+		return func() tea.Msg {
 			err := c.montui.DeleteRule(msg.ID)
 			if err != nil {
-				channel <- NewErrorMsg(err)
-			} else {
-				channel <- GetRulesMsg{}
+				return NewErrorMsg(err)
 			}
-		})
+			return GetRulesMsg{}
+		}
 	case CategoriseMsg:
-		return Future(func(channel chan tea.Msg) {
+		return func() tea.Msg {
 			err := c.montui.CategoriseTransactions()
 			if err != nil {
-				channel <- NewErrorMsg(err)
-			} else {
-				channel <- GetTransactionsMsg{}
+				return NewErrorMsg(err)
 			}
+			return GetTransactionsMsg{}
+		}
 
-		})
 	}
 
 	return nil
-}
-
-type FutureFn func(channel chan tea.Msg)
-
-func Future(fn FutureFn) tea.Cmd {
-	sub := make(chan tea.Msg)
-	go fn(sub)
-
-	return func() tea.Msg {
-		return <-sub
-	}
 }
